b403a0bcd8a694cc88d135ffb892d6dba2a2bf28
teddy0818 <taddy0818@naver.com>
2020-10-13 14:54:31 +0900

    index 페이지 구현 완료git add .

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ var mySwiper = new Swiper('.swiper-container', {
     // 카테고리 탭메뉴
     var tabList = document.querySelectorAll('.tab_list_wrap .tab_list')
 
-    Array.prototype.forEach.call(tabList, function(list) {
-        list.children[0].addEventListener('click', function(e) {
-                e.preventDefault()
+    Array.prototype.forEach.call(tabList, function(list) { /*forEach 사용이 원활히 작동하지 않아 call을 사용 */
+        list.children[0].addEventListener('click', function(e) { 
+                e.preventDefault() /* a 태그가 이동되는 걸 막아줌 */
                 var tabContent = document.querySelectorAll('.tab_container .tab_content')
                 var tabNum = this.parentElement.getAttribute('data-tabnum')
 
@@ -53,4 +53,5 @@ var mySwiper = new Swiper('.swiper-container', {
                 if(list.className.indexOf('tab_active') == -1) {
                     list.className = 'tab_list tab_active'
                 }
-            }) 
+            })
+        })
