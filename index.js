8cb68f19e4622bb75791b2ddb4951b0621b403cc
teddy0818 <taddy0818@naver.com>
2020-10-13 14:18:28 +0900

    index 탭메뉴 구현 중

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
+
+// 슬라이드
 var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
+
     direction: 'horizontal',
     loop: true,
   
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
   })
+
+  // 카테고리 드랍다운
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+    }
+
+    // 카테고리 탭메뉴
+    var tabList = document.querySelectorAll('.tab_list_wrap .tab_list')
+
+    Array.prototype.forEach.call(tabList, function(list) {
+        list.children[0].addEventListener('click', function(e) {
+                e.preventDefault()
+                var tabContent = document.querySelectorAll('.tab_container .tab_content')
+                var tabNum = this.parentElement.getAttribute('data-tabnum')
+
+                Array.prototype.forEach.call(tabContent, function(cont, i) {
+                cont.style.display = 'none'
+                tabList[i].className = 'tab_list'
+                })
+                
+                tabContent[tabNum].style.display = 'block'
+                
+                if(list.className.indexOf('tab_active') == -1) {
+                    list.className = 'tab_list tab_active'
+                }
+            }) 
