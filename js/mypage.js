73f21584b007f6e3e7d482ae3ebcf66dde4dfcda
teddy0818 <taddy0818@naver.com>
2020-10-23 12:35:35 +0900

    mypage - modal 수정

--- js/mypage.js
+++ js/mypage.js
@@ -20,3 +20,27 @@ Array.prototype.forEach.call(tabList, function(list) {
         })
     })
 
+var modal = document.querySelector(".myModal");
+var btn = document.querySelector(".review");
+var closebtn = document.querySelector(".close");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+closebtn.onclick = function() {
+    var txt = document.querySelector('textarea').value
+    if(txt.length == '' || txt.length == 0){
+        alert('후기를 작성해 주세요.')
+        return false
+    }
+    if(txt.length != '' || txt.length != 0){
+        modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
